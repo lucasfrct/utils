e10e37d7159bf6093a4e6487011fcb7c2b6146fe
lucasfrct <lucasfrct@gmail.com>
2022-02-07 09:08:50 -0300

    update

--- environment-go/main.go
+++ environment-go/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	fmt.Println("TEST")
+	gopath := os.Getenv("GOPATH")
+	gobin := os.Getenv("GOBIN")
+	goprivate := os.Getenv("GOPRIVATE")
+	goroot := os.Getenv("GOROOT")
+
+	fmt.Println("GOPATH:", gopath)
+	fmt.Println("GOBIN", gobin)
+	fmt.Println("GOPRIVATE: ", goprivate)
+	fmt.Println("GOROOT: ", goroot)
+
 }
