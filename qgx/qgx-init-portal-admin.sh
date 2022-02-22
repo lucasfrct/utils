853bebb908f0327165ded20aa08e3e61e912c355
Your Name <lucasfrct@gmail.com>
2022-02-22 09:12:40 -0300

    update

--- qgx/qgx-init-portal-admin.sh
+++ qgx/qgx-init-portal-admin.sh
@@ -4,7 +4,7 @@ sleep 1
 gnome-terminal --tab --title="ADMIN ACCOUNT" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-portal-admin-account-service-node; npm run dev;
 $SHELL'"
 sleep 1
-gnome-terminal --tab --title="ADMIN TRANSACTION" --command="bash -c 'cd ~/development/repository/ qgx-payment-ip-portal-admin-transaction-service-node; npm run dev;
+gnome-terminal --tab --title="ADMIN TRANSACTION" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-portal-admin-transaction-service-node; npm run dev;
 $SHELL'"
 sleep 1
 gnome-terminal --tab --title="ADMIN ANGULAR" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-portal-admin-angular; npm start;
