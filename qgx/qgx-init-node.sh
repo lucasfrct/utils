f8317d8bd240294dff4e5584fa7916ceb1f16e15
Your Name <lucasfrct@gmail.com>
2022-02-22 11:06:50 -0300

    update

--- qgx/qgx-init-node.sh
+++ qgx/qgx-init-node.sh
@@ -1,8 +1,8 @@
-gnome-terminal --tab --title="CREDENTIALS NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-credentials-service-node; ./local.sh; 
+gnome-terminal --tab --title="CREDENTIALS NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-credentials-service-node; npm run dev; 
 $SHELL'" 
 sleep 1
-gnome-terminal --tab --title="ACCOUNT NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-account-service-node; ./local.sh; 
+gnome-terminal --tab --title="ACCOUNT NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-account-service-node; npm run dev; 
 $SHELL'" 
 sleep 1
-gnome-terminal --tab --title="PUBLIC NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-public-service-node; ./local.sh;
+gnome-terminal --tab --title="PUBLIC NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-public-service-node; npm run dev;
 $SHELL'"
