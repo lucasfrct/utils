dd51dc0b035e3588148dbeeeacb1b309cccf1b30
Your Name <lucasfrct@gmail.com>
2022-03-11 11:40:48 -0300

    update

--- qgx/mysql/add-created-by.in.transactions_bankslip.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `qgx_baas`.`transactions_bankslip` 
-ADD COLUMN `created_by_uuid` VARCHAR(255)
-AFTER `description`,
-ADD COLUMN `created_by_info` VARCHAR(255) 
-AFTER `created_by_uuid`;
