fb3fa63bf2b7738a7aa12fdb8bd06cc96d3e72c4
Your Name <lucasfrct@gmail.com>
2022-03-17 17:01:21 -0300

    update

--- qgx/mysql/add-created-by.tabe,sql.sql
+++ qgx/mysql/add-created-by.tabe,sql.sql
@@ -1,4 +1,10 @@
 ALTER TABLE `qgx_baas`.`transactions_bankslip` 
-ADD COLUMN `created_by_document` VARCHAR(255),
-ADD COLUMN `created_by_info` VARCHAR(255) 
-AFTER `description`;
+DROP COLUMN `created_by_document`,
+DROP COLUMN `created_by_info`;
+
+ALTER TABLE `qgx_baas`.`transactions_bankslip` 
+ADD COLUMN `created_by_document` VARCHAR(14)
+AFTER `description`,
+ADD COLUMN `created_by_info` VARCHAR(100) 
+AFTER `created_by_document`;
+
