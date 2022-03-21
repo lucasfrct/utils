08251b29e3e811cd51b2a8e12c4a7ba372ce118f
Your Name <lucasfrct@gmail.com>
2022-03-21 18:29:54 -0300

    update

--- qgx/mysql/connect-jumpbox.sh
+++ qgx/mysql/connect-jumpbox.sh
@@ -1,1 +1,2 @@
-docker exec -it mysql bash mysql -h localhost -u root -p qgxDash12345
+# docker exec -it mysql bash mysql -h localhost -u root -p qgxDash12345
+docker exec -it mysql sh
--- qgx/qgx-init-webapp.sh
+++ qgx/qgx-init-webapp.sh
@@ -1,1 +1,3 @@
 gnome-terminal --tab --title="WEBAPP ANGULAR" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-webapp-angular; npm start; $SHELL'"
+gnome-terminal --tab --title="BALANCE" --command="bash -c 'cd ~/go/src/github.com/qgx-pagamentos/qgx-payment-ip-balance-service-go; ./local.sh; $SHELL'"
+gnome-terminal --tab --title="ESCROW" --command="bash -c 'cd ~/go/src/github.com/qgx-pagamentos/qgx-payment-ip-escrow-service-go; ./local.sh; $SHELL'"
