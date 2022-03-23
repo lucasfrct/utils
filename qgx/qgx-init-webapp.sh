300bf668ecdcf693565aba28916c0c6fd72cbf98
Your Name <lucasfrct@gmail.com>
2022-03-23 16:24:32 -0300

    update

--- qgx/qgx-init-webapp.sh
+++ qgx/qgx-init-webapp.sh
@@ -3,3 +3,4 @@ gnome-terminal --tab --title="BALANCE" --command="bash -c 'cd ~/go/src/github.co
 gnome-terminal --tab --title="ESCROW" --command="bash -c 'cd ~/go/src/github.com/qgx-pagamentos/qgx-payment-ip-escrow-service-go; ./local.sh; $SHELL'"
 gnome-terminal --tab --title="TRANSFERS" --command="bash -c 'cd ~/go/src/github.com/qgx-pagamentos/qgx-payment-ip-transfers-service-go; ./local.sh; $SHELL'"
 gnome-terminal --tab --title="FAVORED" --command="bash -c 'cd ~/go/src/github.com/qgx-pagamentos/qgx-payment-ip-favored-service-go; ./local.sh; $SHELL'"
+gnome-terminal --tab --title="PIX" --command="bash -c 'cd ~/go/src/github.com/qgx-pagamentos/qgx-payment-ip-pix-client-service-go; ./local.sh; $SHELL'"
