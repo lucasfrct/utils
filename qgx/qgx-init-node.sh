0c22e2de5d0b4c8504f3c977711e721d789488e0
Your Name <lucasfrct@gmail.com>
2022-04-04 16:49:10 -0300

    update

--- qgx/qgx-init-node.sh
+++ qgx/qgx-init-node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash +x
 
+pkill -f node;
+
 gnome-terminal --tab --title="CREDENTIALS NODE" --command="bash -c 'cd ~/development/repository/qgx-payment-ip-credentials-service-node; npm run dev; 
 $SHELL'" 
 sleep 1
