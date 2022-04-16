6d9466c56952b4b93c6cbc3be419abb0fbff59d7
lucasfrct <lucasfrct@gmail.com>
2022-04-16 12:20:04 -0300

    add arch

---
+++ environment-arch/github.config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash +x
+
+yes | sudo pacman -Sy openssh
+git config --global user.name "lucasfrct"
+git config --global user.email "lucasfrct@gmail.com"
+git config --global color.ui true
+git config --global core.editor "code --wait"
+git config --global merge.conflictstyle diff3
+ssh-keygen -t rsa -C "lucasfrct@gmail.com"
+cat ~/.ssh/id_rsa.pub
+ssh -T git@github.com
