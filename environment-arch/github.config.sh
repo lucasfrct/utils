cf415c3dfb3f5ad03421255e23b1384cddcda106
lucasfrct <lucasfrct@gmail.com>
2022-04-16 15:29:25 -0300

    update

--- environment-arch/github.config.sh
+++ environment-arch/github.config.sh
@@ -6,6 +6,6 @@ git config --global user.email "lucasfrct@gmail.com"
 git config --global color.ui true
 git config --global core.editor "code --wait"
 git config --global merge.conflictstyle diff3
-ssh-keygen -t rsa -C "lucasfrct@gmail.com"
+# ssh-keygen -t rsa -C "lucasfrct@gmail.com"
 cat ~/.ssh/id_rsa.pub
 ssh -T git@github.com
