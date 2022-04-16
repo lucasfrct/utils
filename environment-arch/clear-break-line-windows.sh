5281dd486da19a9057d49e60cba0a6ef5e72d6a3
lucasfrct <lucasfrct@gmail.com>
2022-04-16 15:24:21 -0300

    update

--- environment-arch/clear-break-line-windows.sh
+++ environment-arch/clear-break-line-windows.sh
@@ -2,3 +2,6 @@
 
 ## removem  fim de linha do windows 
 sed -i 's/\r$//' ./clean-intall.sh
+find . -type d -print0 | xargs -0 chmod 755  
+find . -type f -print0 | xargs -0 chmod 644 
+find . -type f -name "*.sh" -exec chmod +x {} \; 
