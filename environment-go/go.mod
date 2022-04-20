e21c0f4d8761e783e4ff68f59f0a15f3a7b75335
lucasfrct <lucasfrct@gmail.com>
2022-04-20 14:52:35 -0300

    Update environmento go

--- environment-go/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/environment-go
-
-go 1.17
-
-require github.com/google/uuid v1.3.0
--- environment-go/main.go
+++ environment-go/main.go
@@ -8,19 +8,25 @@ import (
 func main() {
 
 	port := os.Getenv("GOPORT")
+	project := os.Getenv("PATH_PROJECT")
+	path := os.Getenv("PATH")
 
 	gopath := os.Getenv("GOPATH")
 	gobin := os.Getenv("GOBIN")
+	gopkg := os.Getenv("GOPKG")
+	gosrc := os.Getenv("GOSRC")
 	goprivate := os.Getenv("GOPRIVATE")
 	goroot := os.Getenv("GOROOT")
-	gopkg := os.Getenv("GOPKG")
 
 	fmt.Println("")
 	fmt.Println("PORT: ", port)
 	fmt.Println("GOPATH: ", gopath)
 	fmt.Println("GOBIN: ", gobin)
 	fmt.Println("GOPKG: ", gopkg)
+	fmt.Println("GOSRC: ", gosrc)
 	fmt.Println("GOROOT: ", goroot)
 	fmt.Println("GOPRIVATE: ", goprivate)
+	fmt.Println("PATH_PROJECT: ", project)
+	fmt.Println("PATH: ", path)
 
 }
