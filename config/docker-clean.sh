ce991b3ca6a12f2b65ad7c16c858d8a6e57bf522
lucasfrct <lucasfrct@gmail.com>
2022-05-07 23:57:49 -0300

    update

---
+++ config/docker-clean.sh
@@ -0,0 +1,24 @@
+
+## para todos os cotainers
+# sudo docker kill $(docker ps -q)
+
+# ## remove todos os containers orphans
+sudo docker-compose down --remove-orphans
+
+# ## deleta todos os containers
+sudo docker rm $(docker ps -a -q)
+sudo docker container prune -f
+
+# ## deleta todos os volumes
+sudo docker volume prune -f
+
+# ## delete todas as redes
+sudo docker network prune -f
+
+# ## deleta rodas a imagens
+sudo docker rmi $(docker images -q)
+sudo docker image prune -f
+sudo docker image prune -a -f
+
+# ## Limpa todos os sistemas do docker
+sudo docker system prune -f
