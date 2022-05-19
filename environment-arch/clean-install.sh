e12a4cf1d07eec725af5ee19e950edbc3d5f6899
lucasfrct <lucasfrct@gmail.com>
2022-05-19 16:31:54 -0300

    update

--- environment-arch/clean-install.sh
+++ environment-arch/clean-install.sh
@@ -2,21 +2,21 @@
 
 source add-user-sudo.sh
 source init-archlinux.sh
-# source install-yay.sh
-# source install-yaourt.sh
-# source install-zsh.sh
-# source install-asdf.sh
+source install-yay.sh
+source install-yaourt.sh
+source install-zsh.sh
+source install-asdf.sh
 source install-tools-basic.sh
-# source install-tools.sh
-# source install-docker.sh
-# source install-c.sh
-# source install-rust.sh
-# source install-golang.sh
-# source install-python.sh
-# source install-java.sh
-# source install-node.sh
-# source install-flutter.sh
-# source install-github.sh
-# source github-config.sh
+source install-tools.sh
+source install-docker.sh
+source install-c.sh
+source install-rust.sh
+source install-golang.sh
+source install-python.sh
+source install-java.sh
+source install-node.sh
+source install-flutter.sh
+source install-github.sh
+source github-config.sh
 # source alias.sh
 
